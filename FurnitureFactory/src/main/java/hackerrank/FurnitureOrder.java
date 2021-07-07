4d64dacc3972b2445cc14d881c5501683805dbfa
ankitasak <87056649+ankitasak@users.noreply.github.com>
2021-07-07 10:55:30 +0530

    Update FurnitureOrder.java

--- FurnitureFactory/src/main/java/hackerrank/FurnitureOrder.java
+++ FurnitureFactory/src/main/java/hackerrank/FurnitureOrder.java
@@ -12,34 +12,58 @@ public class FurnitureOrder implements FurnitureOrderInterface {
      */
     FurnitureOrder() {
         // TODO: Complete the constructor
+        furnitures=new HashMap<Furniture ,Integer>();
     }
 
     public void addToOrder(final Furniture type, final int furnitureCount) {
         // TODO: Complete the method
+        Integer cnt=0;
+        if(furnitures.containsKey(type)){
+            cnt=furnitures.get(type);
+        }
+        furnitures.put(type,cnt+funitureCount)
     }
 
     public HashMap<Furniture, Integer> getOrderedFurniture() {
         // TODO: Complete the method
-        return null;
+        
+        return new HashMap<Furniture,Integer>(furnitures);
     }
 
     public float getTotalOrderCost() {
         // TODO: Complete the method
-        return -1.0f;
+        if(!furnitures.isEmpty())
+        {
+            return furnitures.entrySet().stream().mao(f->f.getKey.cost()*f.getValue()).collect (Collectors.toList()).strem().reduce(Float::sum).get();
+        }
+    
+        return 0.0f;
     }
 
     public int getTypeCount(Furniture type) {
         // TODO: Complete the method
-        return -1;
+        if(furnitures.containsKey(type))
+        {
+            return furnitures.get(type);
+        }
+        return 0;
     }
 
     public float getTypeCost(Furniture type) {
         // TODO: Complete the method
-        return -1.0f;
+               if(furnitures.containsKey(type))
+        {
+            return furnitures.get(type)*type.cost();
+        }
+        return 0.0f;
     }
 
     public int getTotalOrderQuantity() {
         // TODO: Complete the method
-        return -1;
+          if(!furnitures.isEmpty())
+        {
+              return furnitures.values().stream().reduce(Integer::sum).get();
+        
+        return 0;
     }
 }
